307f33a37732627ec2610829b7c10fe5db8b9fe4
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-05-22 15:03:01 -0500

    Added function to write mission statement to DOM

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-let message = "";
 
 
+const write = () => {
+  let main = document.querySelector("article");
+  let mission = document.createElement("h4");
+  mission.textContent = "Representing people: it's what I do.";
+  main.appendChild(mission);
+}
+
+write();
