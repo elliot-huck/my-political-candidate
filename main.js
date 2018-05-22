329a6aa5b53e1a4cc2935d2a2fe19c12f8376cb0
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-05-22 15:35:34 -0500

    Added the function to create and set district attribute

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+//some global variables
+const districtNum = 5;
+const main = document.querySelector("article");
 
-
+// writes a made up mission statement to the <article> element
 const write = () => {
-  let main = document.querySelector("article");
-  let mission = document.createElement("h4");
+  const mission = document.createElement("h4");
   mission.textContent = "Representing people: it's what I do.";
   main.appendChild(mission);
 }
 
+// creates an attribute to store the congressional district number & writes it to the article
+const setDistrict = (district) => {
+  let newAttr = document.createAttribute("district");
+  newAttr.value = district;
+  console.log(newAttr.value);
+  main.setAttributeNode(newAttr);
+} 
+
 write();
+setDistrict(districtNum);
